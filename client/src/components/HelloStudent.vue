da3e3f14514ffccff0d4e53525c506b54dd48368
L00177572 AE <L00177572@atu.ie>
2022-12-20 22:40:06 +0000

    webApp - initial commit

---
+++ client/src/components/HelloStudent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="HelloStudent">
+    <h1>Hello Student</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloStudent",
+}
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="LoginForm">
+    <br />
+    <v-img contain max-height="150" max-width="300" :src="require('../assets/logo.png')"></v-img>
+    <h1 class="text-center">Login Form</h1>
+    <br />
+    <v-form ref="form">
+      <v-text-field v-model="email" type="email" label="E-mail" required></v-text-field>
+
+      <v-text-field v-model="password" type="password" label="E-mail" required></v-text-field>
+
+      <v-btn color="error" class="mr-4" @click="login"> Login </v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data() {
+    return {
+      email: "",
+      password: "",
+    }
+  },
+  methods: {
+    login() {
+      console.log("AE: ~ login")
+      this.$router.push({ name: "home" })
+      this.$root.loggedUser = {}
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
