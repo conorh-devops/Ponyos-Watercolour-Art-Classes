749f7dafbb3ffd20d95a8fc76744a1ac19656f4c
L00177572 AE <L00177572@atu.ie>
2023-01-24 22:18:42 +0000

    PWA-4

--- client/src/views/Courses.vue
+++ client/src/views/Courses.vue
@@ -7,7 +7,9 @@
       <v-card class="mx-auto" max-width="344" v-for="course in Object.keys($root.courses)" :key="course">
         <v-card-text>
           <p class="text-h4 text--primary" v-text="course" />
-          <p>Description</p>
+          <p class="text-h7"
+            v-text="'Duration: ' + $root.courses[course].duration + 'hrs | Price: €' + $root.courses[course].price" />
+          <p class="text-h7">Description</p>
           <div class="text--primary" v-text="$root.courses[course].desc" />
         </v-card-text>
         <v-card-actions>
@@ -45,6 +47,26 @@
       </v-card>
     </v-dialog>
 
+    <v-dialog v-model="dialog.show" persistent max-width="500">
+      <v-card>
+        <v-card-title class="text-h5">
+          Ponyo’s Watercolour Art Classes
+        </v-card-title>
+        <v-card-text>
+          <span v-html="dialog.content" />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn v-if="dialog.cancelShow" text id="btnCancel" @click="confirmingModal(false)">
+            Cancel
+          </v-btn>
+          <v-btn color="primary" text id="btnConfirm" @click="confirmingModal(true)">
+            Confirm
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
 
@@ -59,8 +81,9 @@ export default {
       dialog: {
         show: false,
         content: "",
-        isEnrolled: false
-      }
+        isEnrolled: false,
+        isFeedbackMessage: false
+      },
     }
   },
   beforeRouteEnter(_to, _from, next) {
@@ -77,6 +100,8 @@ export default {
       const enrolled = Boolean(this.getEnrolledDate(course))
       this.dialog.course = course
       this.dialog.isEnrolled = enrolled
+      this.dialog.cancelShow = true
+      this.dialog.isFeedbackMessage = false
       this.dialog.content = this.dialog.isEnrolled ?
         `Are you sure you want to <b>remove</b> "${this.dialog.course}" form your Enrolloed list?` :
         `Are you sure you want to <b>enroll</b> "${this.dialog.course}"?`
@@ -84,8 +109,9 @@ export default {
     },
     async confirmingModal(confirmed) {
       this.dialog.show = false
-      if (!confirmed) return
+      if (this.dialog.isFeedbackMessage) return
 
+      if (!confirmed) return
 
       if (this.dialog.isEnrolled)
         delete this.$root?.loggedUser?.courses[this.dialog.course]
@@ -96,7 +122,14 @@ export default {
       if (result.status !== 200)
         return window.alert(`Something went wrong. Code: cd3c2327. Error: ${result.message || JSON.stringify(result)}`)
 
-      window.alert(`Saved`)
+      setTimeout(() => {
+        this.dialog.cancelShow = false
+        this.dialog.isFeedbackMessage = true
+        this.dialog.content =
+          `You've successfully <b id="successAction">${this.dialog.isEnrolled ? "removed" : "enrolled"}</b> the course`
+        this.dialog.show = true
+      }, 500)
+
     }
   },
 }
