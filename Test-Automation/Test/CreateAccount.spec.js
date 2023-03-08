60db7670816d834818b5a93f522a8f71afd35fc5
l00177583 <l00177583@atu.ie>
2023-03-08 22:12:00 +0000

    Updated Automation test

--- Test-Automation/Test/CreateAccount.spec.js
+++ Test-Automation/Test/CreateAccount.spec.js
@@ -1,60 +1,72 @@
 const { test, expect } = require("@playwright/test");
 
-test('Register user account', async ({ page }) => {
-    await page.goto('http://ponyos-watercolour-art-classes-dev.s3-website-eu-west-1.amazonaws.com/#/home');
-    await expect(page).toHaveTitle('art-class');
-    await page.getByRole('button', { name: 'Log In' }).click();
-    await page.getByRole('button', { name: 'Sign up' }).click();
-    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').click();
-    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').fill('atu@atu.ie');
-    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').press('Tab');
-    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldPassword').fill('password');
-    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldPassword').press('Tab');
-    await page.getByLabel('Password Confirmation').fill('password');
-    await page.getByLabel('Password Confirmation').press('Tab');
-    await page.getByLabel('Your Name').fill('atuDevOps');
-    await page.getByRole('button', { name: 'Sign up' }).click();
-    // await page.goto('https://ponyos-watercolour-artclasses-bucket.s3.eu-north-1.amazonaws.com/index.html#/courses')
-    // await page.waitForTimeout(3000)
+{
 
-})
+    let rand1 = (Math.random() + 1).toString(36).substring(7);
+    var email = (rand1 + "@atu.ie");
+    // console.log(email);
 
-test('Verify student can Enroll in the specific course', async ({ page }) => {
-    await page.goto('http://ponyos-watercolour-art-classes-dev.s3-website-eu-west-1.amazonaws.com/#/home')
-    await page.getByRole('button', { name: 'Log In' }).click();
-    await page.getByLabel('E-mail').click();
-    await page.getByLabel('E-mail').click();
-    await page.getByLabel('E-mail').fill('atu@atu.ie');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('password');
-    await page.getByRole('button', { name: 'Login' }).click();
-    await page.getByRole('button', { name: 'Courses' }).click();
-    await page.getByRole('button', { name: 'Enroll' }).click();
-    await page.getByRole('button', { name: 'Cancel' }).click();
-    await page.getByRole('button', { name: 'Enroll' }).click();
-    await page.getByRole('button', { name: 'Confirm' }).click();
-    await page.getByRole('button', { name: 'Confirm' }).click();
-});
+    test('Register user account', async ({ page }) => {
 
-test('Verify user student can unenroll from the specific course', async ({ page }) => {
+        await page.goto('http://ponyos-watercolour-art-classes-dev.s3-website-eu-west-1.amazonaws.com/#/home');
+        await expect(page).toHaveTitle('art-class');
+        await page.getByRole('button', { name: 'Log In' }).click();
+        await page.getByRole('button', { name: 'Sign up' }).click();
+        await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').click();
+        await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').fill(email);
+        await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').press('Tab');
+        await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldPassword').fill('password');
+        await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldPassword').press('Tab');
+        await page.getByLabel('Password Confirmation').fill('password');
+        await page.getByLabel('Password Confirmation').press('Tab');
+        await page.getByLabel('Your Name').fill('atuDevOps');
+        await page.getByRole('button', { name: 'Sign up' }).click();
+        await page.getByRole('button', { name: 'Go to Home' }).click();
+        await page.waitForTimeout(2000);
 
-    await page.goto('http://ponyos-watercolour-art-classes-dev.s3-website-eu-west-1.amazonaws.com/#/home')
-    await page.getByRole('button', { name: 'Log In' }).click();
-    await page.getByLabel('E-mail').click();
-    await page.getByLabel('E-mail').click();
-    await page.getByLabel('E-mail').fill('atu@atu.ie');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('password');
-    await page.getByRole('button', { name: 'Login' }).click();
-    await page.getByRole('button', { name: 'Courses' }).click();
-    await page.waitForTimeout(6000);
-    await page.locator('#btnRemove_Fujimoto').click();
-    await page.getByRole('button', { name: 'Cancel' }).click();
-    await page.locator('#btnRemove_Fujimoto').click();
-    await page.getByRole('button', { name: 'Confirm' }).click();
-    await page.getByRole('button', { name: 'Confirm' }).click();
+    })
 
-});
+    test('Verify student can Enroll in the specific course', async ({ page }) => {
+        await page.goto('http://ponyos-watercolour-art-classes-dev.s3-website-eu-west-1.amazonaws.com/#/home')
+        await page.getByRole('button', { name: 'Log In' }).click();
+        await page.getByLabel('E-mail').click();
+        await page.getByLabel('E-mail').click();
+        await page.getByLabel('E-mail').fill(email);
+        await page.getByLabel('Password').click();
+        await page.getByLabel('Password').fill('password');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await page.waitForTimeout(2000);
+        await page.getByRole('button', { name: 'Courses' }).click();
+        await page.waitForTimeout(2000);
+        await page.locator('#btnEnroll_Sosuke').click();
+        // await page.getByRole('button', { name: 'Enroll' }).click();
+        await page.getByRole('button', { name: 'Cancel' }).click();
+        // await page.getByRole('button', { name: 'Enroll' }).click();
+        await page.locator('#btnEnroll_Sosuke').click();
+        await page.getByRole('button', { name: 'Confirm' }).click();
+        await page.getByRole('button', { name: 'ok' }).click();
+    });
 
+    test('Verify user student can unenroll from the specific course', async ({ page }) => {
 
+        await page.goto('http://ponyos-watercolour-art-classes-dev.s3-website-eu-west-1.amazonaws.com/#/home')
+        await page.getByRole('button', { name: 'Log In' }).click();
+        await page.getByLabel('E-mail').click();
+        await page.getByLabel('E-mail').click();
+        await page.getByLabel('E-mail').fill(email);
+        await page.getByLabel('Password').click();
+        await page.getByLabel('Password').fill('password');
+        await page.getByRole('button', { name: 'Login' }).click();
+        await page.getByRole('button', { name: 'Courses' }).click();
+        await page.waitForTimeout(2000);
+        // await page.getByRole('button', { name: 'Remove' }).click();
+        await page.locator('#btnRemove_Sosuke').click();
+        await page.getByRole('button', { name: 'Cancel' }).click();
+        await page.locator('#btnRemove_Sosuke').click();
+        await page.getByRole('button', { name: 'Confirm' }).click();
+        await page.getByRole('button', { name: 'Ok' }).click();
+
+    });
+
+};
 
