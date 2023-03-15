eb94ee8538e280c3484ab70cca45d445ded364f5
l00177583 <l00177583@atu.ie>
2023-03-15 21:11:41 +0000

    updated email and Wait tile

--- Test-Automation/Test/CreateAccount.spec.js
+++ Test-Automation/Test/CreateAccount.spec.js
@@ -13,7 +13,7 @@ test('Register user account', async ({ page }) => {
     await page.getByRole('button', { name: 'Log In' }).click();
     await page.getByRole('button', { name: 'Sign up' }).click();
     await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').click();
-    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').fill('TestATU@atu.ie');
+    await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').fill(email);
     await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldEmail').press('Tab');
     await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldPassword').fill('password');
     await page.locator('form').filter({ hasText: 'E-mailPasswordPassword ConfirmationYour NameSign up' }).locator('#fldPassword').press('Tab');
@@ -21,8 +21,9 @@ test('Register user account', async ({ page }) => {
     await page.getByLabel('Password Confirmation').press('Tab');
     await page.getByLabel('Your Name').fill('atuDevOps');
     await page.getByRole('button', { name: 'Sign up' }).click();
-    await page.getByRole('button', { name: 'Go to Home' }).click();
     await page.waitForTimeout(2000);
+    await page.getByRole('button', { name: 'Go to Home' }).click();
+
 
 })
 
